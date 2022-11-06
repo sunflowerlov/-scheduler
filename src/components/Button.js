fa8b0810b358c3d0ce94a8be8246e9e70ba2b907
sunflowerlov <lishaliang1999@gmail.com>
2022-11-06 08:33:02 -0500

    involve className library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+   classNames(buttonClass, {"button--confirm": props.confirm});
+   classNames(buttonClass, {"button--danger": props.danger});
+
+    return (
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    );
+ }
+ 
