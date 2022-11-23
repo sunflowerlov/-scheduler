21f56b137f0e3f5a9fa47c49a81883c8090eb9de
sunflowerlov <lishaliang1999@gmail.com>
2022-11-23 18:37:01 -0500

    refractor

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,72 +8,17 @@ import {
   getInterview,
   getInterviewersForDay,
 } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  const setDay = function (day) {
-    setState((prev) => ({ ...prev, day }));
-  };
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
-    ]).then(([days, appointments, interviewers]) => {
-      setState((prev) => ({
-        ...prev,
-        days: days.data,
-        appointments: appointments.data,
-        interviewers: interviewers.data,
-      }));
-    });
-  }, []);
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-
-  function bookInterview(id, interview) {
-    return axios
-      .put(`/api/appointments/${id}`, { interview })
-      .then((response) => {
-        const appointment = {
-          ...state.appointments[id],
-          interview: { ...interview },
-        };
-
-        const appointments = {
-          ...state.appointments,
-          [id]: appointment,
-        };
-
-        setState({
-          ...state,
-          appointments,
-        });
-      });
-  }
-
-  function cancelInterview(id) {
-    return axios.delete(`/api/appointments/${id}`).then(() => {
-      const appointment = {
-        ...state.appointments[id],
-        interview: null,
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment,
-      };
-      setState({
-        ...state,
-        appointments,
-      });
-    });
-  }
 
   return (
     <main className="layout">
